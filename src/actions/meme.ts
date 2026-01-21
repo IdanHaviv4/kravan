5dde6a5f74bd69a0301cc0b826e6be0655a4de4f
havividan4 <havividan4@gmail.com>
2026-01-21 14:37:42 +0200

    adding cooking meme

--- src/actions/meme.ts
+++ src/actions/meme.ts
@@ -9,6 +9,7 @@ import { CustomEmbed } from "../utils/embed.js";
 
 enum Memes {
   TAKE_MONEY,
+  COOKING,
 }
 
 type MemeT = {
@@ -18,17 +19,30 @@ type MemeT = {
 };
 
 export class Meme {
-  static MEMES: Map<Memes, MemeT> = new Map().set(Memes.TAKE_MONEY, {
-    name: "Take my money",
-    image: "https://imgflip.com/s/meme/Shut-Up-And-Take-My-Money-Fry.jpg",
-    avatars: [
-      {
-        x: 133,
-        y: 58,
-        size: 136,
-      },
-    ],
-  });
+  static MEMES: Map<Memes, MemeT> = new Map<Memes, MemeT>()
+    .set(Memes.TAKE_MONEY, {
+      name: "Take my money",
+      image: "https://imgflip.com/s/meme/Shut-Up-And-Take-My-Money-Fry.jpg",
+      avatars: [
+        {
+          x: 133,
+          y: 58,
+          size: 136,
+        },
+      ],
+    })
+    .set(Memes.COOKING, {
+      name: "Cooking",
+      image:
+        "https://instagram.ftlv6-1.fna.fbcdn.net/v/t51.29350-15/394556382_287308694224822_9173530909992844440_n.jpg?stp=dst-jpg_e35_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6IkZFRUQuaW1hZ2VfdXJsZ2VuLjc1MHg3NDQuc2RyLmYyOTM1MC5kZWZhdWx0X2ltYWdlLmMyIn0&_nc_ht=instagram.ftlv6-1.fna.fbcdn.net&_nc_cat=110&_nc_oc=Q6cZ2QGupJt_yqAMObPHvWQDJR6kByHuCRbWzV3_xW0vsj2ZwOf7IgLYPyjTgUKcseQll-s&_nc_ohc=l--Iye8Wm9kQ7kNvwEpAxC6&_nc_gid=dT4xlwvtRN_bxUlfjrtSJg&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzIyMDMwNjEzNzAzMDc3NjI2OA%3D%3D.3-ccb7-5&oh=00_AfofUGJugiFm2qdv86UF4ZyWhz2aInhWtBToK5r37ipjoA&oe=69769A49&_nc_sid=10d13b",
+      avatars: [
+        {
+          x: 145,
+          y: 240,
+          size: 128,
+        },
+      ],
+    });
 
   #meme: MemeT;
   #users: User[];
